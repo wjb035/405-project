6645ce8eafe5dc30b07db059287f276849c88319
tax evasion <keahi.temple@gmail.com>
2026-04-16 14:45:20 -0500

    did i never push this? vairous transition stuff im working on it sitll tho

--- pg-emu/Helpers/ScreenTransition.cs
+++ pg-emu/Helpers/ScreenTransition.cs
@@ -5,13 +5,29 @@ namespace PGEmu.Helpers;
 
 public partial class ScreenTransition : CanvasLayer
 {
+	public enum TransitionType { Fade, Spiral, Noise, Wipe, Radial }
+	
 	private ColorRect _fadeRect = null!;
-
+	private ShaderMaterial _mat = null!;
+	
 	public override void _Ready()
 	{
 		_fadeRect = GetNode<ColorRect>("FadeRect");
 		_fadeRect.Modulate = new Color(0, 0, 0, 0);
+		_mat = new ShaderMaterial();
+		_mat.Shader = GD.Load<Shader>("res://ShaderSlop/TransitionEffect.gdshader");
+		_mat.SetShaderParameter("luminance_cutoff", 0.0f);
+		_fadeRect.Material = _mat;
 	}
+	
+	private Texture2D GetMaskForTransition(TransitionType type) => type switch
+	{
+		TransitionType.Spiral => GD.Load<Texture2D>("res://UI/TransitionTextures/finalspiral.tres"),
+		TransitionType.Noise  => GD.Load<Texture2D>("res://UI/TransitionTextures/noise.tres"),
+		TransitionType.Wipe   => GD.Load<Texture2D>("res://UI/TransitionTextures/wipe.tres"),
+		TransitionType.Radial => GD.Load<Texture2D>("res://Textures/Transitions/radial.png"),
+		_                     => GD.Load<Texture2D>("res://Textures/Transitions/fade.png"), 
+	};
 
 	public async Task FadeOut(float duration = 0.5f)
 	{
