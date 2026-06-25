867dcfe78c11a341731cae4b7b6a980e89a2ecf7
NotJourneyHarris <46025723+NotJourneyHarris@users.noreply.github.com>
2026-06-25 13:15:28 -0500

    Delete catui-main directory

--- catui-main/src/emu/libretro/ILibraryLoader.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public interface ILibraryLoader
-{
-	IntPtr LoadLibrary(string path);
-	IntPtr GetProcAddress(IntPtr handle, string functionName);
-	bool FreeLibrary(IntPtr handle);
-	string GetLibraryExtension();
-}
